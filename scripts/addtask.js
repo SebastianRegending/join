f13bbd645e2f17463ac7515a47d56858a620ddef
Jan B. <j.bruchwalski@web.de>
2024-08-29 01:18:01 +0200

    added contacts to addTask

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -4,6 +4,7 @@ let IDs;
 let expanded = false;
 let URL_contacts = "https://join-da080-default-rtdb.europe-west1.firebasedatabase.app/contacts";
 //let BASE_URL = 
+
 function showCheckboxes() {
   let checkboxes = document.getElementById("checkboxes");
   if (!expanded) {
@@ -25,12 +26,16 @@ async function loadContacts() {
 
     IDs = json2arrayIDs(contactsToConvert[n])
     for (let i = 0; i < IDs.length; i++) {
-      console.log(IDs[i]['name'])
+
+      document.getElementById('checkboxes').innerHTML += `
+      <label for="$contact-${i}">
+                       <input type="checkbox" id="$contact-${i}" />${IDs[i]['name']}</label>`;
+      console.log(IDs);
 
     }
 
     n++
-    //fillContacts();
+
   }
 }
 
@@ -42,32 +47,7 @@ function addTask() {
 
 }
 
-async function fillContacts(responseToJson) {
-  let contactsToConvert = json2arrayContacts(responseToJson);
 
-  let n = 0;
-  while (n < contactsToConvert.length) {
-    IDs = json2arrayIDs(contacts[n]);
-    let oneContact = json2arrayIDs(contacts[n]);
-    let letterForCards = oneContact[0]['initials'].charAt(0);
-
-    IDs.sort((a, b) => a.name.localeCompare(b.name));
-    for (let i = 0; i < IDs.length; i++) {
-      document.getElementById('checkboxes').innerHTML +=                    " <label for="one">
-      <input type="checkbox" id="one" />First checkbox</label>
-  <label for="two">
-      <input type="checkbox" id="two" />Second checkbox</label>
-  <label for="three">
-      <input type="checkbox" id="three" />Third checkbox</label>"
-      
-      
-      // `<label for="$contact-${i}">
-      //                   <input type="checkbox" id="$contact-${i}" />${IDs['name']}</label>`;
-      // console.log(IDs['name']);
-    }
-    n++;
-  }
-}
 
 function json2arrayContacts(json) {
   let result = [];
