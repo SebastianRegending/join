daf18060fc6e01f23bbb6240d64b9edba0122747
Sebastian Regending <sebastianr@web.de>
2024-09-24 21:27:03 +0200

    better sign up form validation

--- scripts/register.js
+++ scripts/register.js
@@ -313,3 +313,43 @@ function startLogoAnimationMobile() {
         animatedLogoContainer.classList.add('logo-behind');
     }, 800);
 }
+
+function validateName() {
+    document.getElementById('name').addEventListener('input', function(e) {
+        let name = e.target.value;
+        let pattern = /^[A-Za-z ]+$/;
+        if (name.match(pattern)) {
+          e.target.setCustomValidity('');
+        } else {
+          e.target.setCustomValidity('Please enter name and use letters only');
+        }
+      });
+      console.log(email);
+      
+    }
+
+function validateEmail() {
+document.getElementById('email').addEventListener('input', function(e) {
+    let email = e.target.value;
+    let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email.match(pattern)) {
+      e.target.setCustomValidity('');
+    } else {
+      e.target.setCustomValidity('Please enter a valid email address. Example: user@example.de');
+    }
+  });
+  console.log(email);
+  
+}
+
+function validatePasswordMatch() {
+    const password = document.getElementById('password');
+const confirm = document.getElementById('confirm_password');
+confirm.addEventListener('input', function(event) {
+    if (password.value !== confirm.value) {
+        confirm.setCustomValidity('Passwords do not match.');
+    } else {
+        confirm.setCustomValidity('');
+    }
+});
+}
